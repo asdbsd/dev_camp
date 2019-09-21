0a1544a020fba4407e6ee437b84cae0693dedd94
Dobriyan Petkov <dobriyanp@yahoo.com>
2019-09-21 12:26:02 +0700

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,9 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    TtgViewTool::Renderer.copyright 'Dobriyan Petkov', 'All rights reserved'
+  end
+
 end
