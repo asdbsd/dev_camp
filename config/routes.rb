a9a15ed4f17aa47e30765288d283f0cf61013389
Dobriyan Petkov <dobriyanp@yahoo.com>
2019-09-26 11:55:01 +0700

    implemented authorization rules for portfolio controller and view for drag and drop feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
+
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
-
-
 
   resources :blogs do
     member do
       get :toggle_status
     end
   end
-
 
   get 'about-us', to: 'pages#about'
   get 'contact', to: 'pages#contact'
